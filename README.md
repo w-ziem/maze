1bf464c572feffa4de4629e37b6940185f9a358e
w-ziem <143935791+w-ziem@users.noreply.github.com>
2024-11-30 22:08:45 +0100

    Contiue setup, add helpers for DFS
    Functions added: shuffle to randomly pick order of directions
    is_valid_cell to check if chosen neighbours is valid (inside grid)
    visited_init to initialize array that stroes info wether the cell was visited
    
    also a definition for directions

--- README.md
+++ README.md
@@ -1,1 +1,45 @@
 # maze
+## etapy tworzenia programu
+
+
+
+
+### Jak reprezentować labirynt (np. tablica grid + dodatkowe informacje o ścianach).
+    ############# ###########
+    ############# ###########
+    ###         # ###########
+    ### ######        #######
+    ### ###### ###### #######
+    ### ###### ###### #######
+    ##########        #######
+    ################# #######
+    ################# #######
+
+1 to ściana, zera to korytarze
+
+ ### Jak losować sąsiadów.
+4 kierunki wiec mozna sobie zrobic typedef enum {left, up, right, down} direction
+no i potem losowac srand(time(NULL)) i direction = rand() % 4 aby uzyskac loswy kierunek
+
+
+
+### Jak sprawdzać, czy dany sąsiad jest w granicach i czy został odwiedzony.
+kazda sprawdzana komorka ma row col, wiec row - 1 to do gory, col -1 to w lewo, kombinacje tego dadza nam wszyskie kierunki, wylosowany sąsiad nie może dotykać granic
+
+
+### Jak usunąć ścianę między komórkami.
+komorka->up == 1 => ściana == 0 brak sciany, chyba tak by to mialo sens, ale znowu wracamy do punktu wyjscia moze dalo by sie jakos latwiej to zrobic, żeby nie trzeba bylo pisac tego jako oddzielnej struktury
+
+Łatwietjsza wersja zakłada po prostu 2 tablice 2D : visited i grid. Jeżeli obok siebie stoją 2 zera (spacje) to nie ma między nimi ściany i tyle
+
+
+### tworzenie ścieżek DFS
+
+dodatkowa tablica visited o tych samych wymiarach przechowuje informacje o tym czy komórka została odwiedzona
+
+1. wybierz kmórkę i zaznacz ją jako odwiedzoną
+2. losuj kolejność sąsiadów
+3. jeżeli sąsiad jest w granicach i nieodwiedzony to usun między nim ścianę i przejdź do niego (wywołaj na nim rekurencję)
+4. Jak wszyscy sąsiedzi odwiedzeni to wróć do poprzedniej komórki
+
+Base case?????
--- main.c
+++ main.c
@@ -1,10 +1,60 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 #include "maze.h"
 //random maze gnerator depth first search
 
 
 //na początku generuje pełną planszę (siatkę o podanym wymiarze)
+
+
+typedef enum {up, right, down, left} direction_t; //definicja rodzajów kierunków
+
+
+//sprawdzanie czy komórka jest w granicach labiryntu
+int is_valid_cell(int m, int n, int row, int col, direction_t d){
+        switch(d){
+        case up: return row - 1 > 0 ? 1 : 0;
+        break;
+        case down: return row + 1 < m ? 1 : 0;
+        break;
+        case right: return col + 1 < n ? 1 : 0;
+        break;
+        case left: return  col - 1 > 0 ? 1 : 0;
+        break; 
+        default : return 0;
+    }
+}
+
+//Fisher-Yates Shuffle
+//losowa zmiana kolejności w tablicy kierunków, aby wybierać sąsiadów w DFS losowo
+void shuffle(direction_t arr[]){
+    srand(time(NULL)); //losowe ziarno generatora liczb losowych
+
+    for(int i = 0; i < 4; i++){
+        int j = i + rand() % (4-i);
+
+        direction_t temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }    
+}
+
+
+int** visited_init(int m, int n){
+    int** arr = malloc(m * sizeof(int*));
+    for(int i=0; i < m; i++){
+    arr[i] = malloc(n * sizeof(int));
+    if(!arr[i]){
+        fprintf(stderr, "Błąd alokacji pamięci dla tablicy visited.\n");
+        return NULL;
+    }
+    for(int j = 0; j < n; j++){
+        arr[i][j] = 0;
+    }
+    }
+    return arr;
+}
 
 
 int main(int argc, char **argv){
@@ -16,6 +66,25 @@ int main(int argc, char **argv){
     if(!grid){
         return EXIT_FAILURE;
     }
+
+
+    //inicializacja tablicy visited, bazowo wszytkie komórki nie odwiedzone
+    //deklaracja możliwych kierunków
+    int** visited = visited_init(m, n);
+    int directions[] = {up, right, down, left}; 
+
+
+    //tworzenie otworów wejściowego (lewy górny) i końcowego (prawy dolny)
+    grid[0][1] = 0;
+    grid[m-1][n-2] = 0;
+
+    visited[0][1] = 0;
+    visited[m-1][n-2] = 0;
+
+
+    //DFS
+
+    
 
 
     //test generowania szkieletu labiryntu
